792aee577dcaed199a3b76894332436a1f1b2d64
Oluwadamilola Olaniyi <oludammydy@gmail.com>
2026-06-29 15:33:50 +0100

    Day 1 reboot

---
+++ week-1/day-1/day1.js
@@ -0,0 +1,28 @@
+// Week 1 - Day 1 - Introduction to JavaScript
+// Variables and Data Types
+
+// Declaring variables
+
+const firstName = "Oluwadamilola";   // String
+const lastName = "Olaniyi";        // String
+
+let age = 29; // Number
+
+const profession = "Software Engineer, Public Health Researcher, Cardiac Perfusionist"; // String
+
+const isLearningJavaScript = true; // Boolean
+
+// Displaying the variables in the console
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(profession);
+console.log(isLearningJavaScript);
+
+// Data Types
+// String, Number, Boolean, Undefined, Null, Object, Symbol
+
+console.log(`My name is ${firstName} ${lastName}.`);
+console.log(`I am ${age} years old.`);
+console.log(`Profession: ${profession}`);
+console.log(`Is learning JavaScript: ${isLearningJavaScript}`);
