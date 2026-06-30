ce657f0c3722d4c54adda39a01cc0287610f1a4a
Oluwadamilola Olaniyi <oludammydy@gmail.com>
2026-06-30 13:36:13 +0100

    Week 1 Day 1: Practice variables and template literals

---
+++ week-1/day-1/classwork1.js
@@ -0,0 +1,29 @@
+// Variables classwork
+
+const hospitalName = "Obafemi Awolowo University Teaching Hospital";
+const numberOfDoctors = 120;
+const numberOfNurses = 250;
+const emergencyOpen = true;
+const numberOfBeds = 500;
+
+console.log(`Hospital: ${hospitalName}`);
+console.log(`Number of Doctors: ${numberOfDoctors}`);
+console.log(`Number of Nurses: ${numberOfNurses}`);
+console.log(`Emergency Department Open: ${emergencyOpen}`);
+console.log(`Number of Beds: ${numberOfBeds}`);
+
+
+const firstName = "Oluwadamilola";   // String
+const lastName = "Olaniyi";        // String
+
+let age = 29; // Number
+
+const profession = "Software Engineer, Public Health Researcher, Cardiac Perfusionist"; // String
+
+const isLearningJavaScript = true; // Boolean
+
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof profession);
+console.log(typeof isLearningJavaScript);
