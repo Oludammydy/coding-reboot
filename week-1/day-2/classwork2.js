6361771974d62046115a535d434b751e6b80043e
Oluwadamilola Olaniyi <oludammydy@gmail.com>
2026-07-01 15:25:18 +0100

    Week 1 Day 2: Operators and conditionals

---
+++ week-1/day-2/classwork2.js
@@ -0,0 +1,31 @@
+// Operators & Making Decisions classwork
+
+const patientName = "John";
+const patientAge = 65;
+
+
+if (patientAge >= 18) {
+    console.log("Eligible for Senior Citizen Care.");
+} else {
+    console.log("Standard Adult Care.");
+}
+
+const emergency = true;
+
+if (emergency == true) {
+    console.log("Send patient directly to Emergency Department.");
+} else {
+    console.log("Proceed to Reception.");
+}
+
+const patientWardName = "Mary";
+const patientWardAge = "65"
+const emergencyPatient = true
+
+
+if (patientWardName && patientWardAge && emergencyPatient) {
+    console.log("Eligible for Senior Citizen Care.");
+} else {
+    console.log("Proceed to Emergency Department.");
+}
+
---
+++ week-1/day-2/day2.js
@@ -0,0 +1,52 @@
+// Day 2 coding reboot
+
+// Operators & Making Decisions
+
+const a = 20;
+const b = 5;
+
+console.log(a + b); 
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+
+const patientAge = 17;
+
+console.log(patientAge > 18);
+console.log(patientAge >= 18);
+console.log(patientAge < 18);
+console.log(patientAge == 17);
+console.log(patientAge === 17);
+console.log(patientAge != 18);
+
+
+const hasInsurance = true;
+const emergency = false
+
+console.log(hasInsurance && emergency);
+console.log(hasInsurance || emergency);
+console.log(!emergency);
+
+
+const patientWardAge = 16
+
+if (patientWardAge >= 18) {
+    console.log("Patient is an adult");
+}
+
+
+const patientRoomAge = 16
+
+if (patientRoomAge >= 18) {
+    console.log("Patient is an adult.");
+} else {
+    console.log("Patient is a minor.");
+}
+
+
+
+const isGreaterThan = a > b;
+const isLessThan = a < b;
+
